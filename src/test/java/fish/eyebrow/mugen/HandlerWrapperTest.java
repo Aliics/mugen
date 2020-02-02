79481e4b56274ac345390476c7eeb804862abb04
Alexander Johnston <Aliics@hotmail.com>
2020-02-03 09:18:03 +1300

    Add sanity test cases

--- src/test/java/fish/eyebrow/mugen/HandlerWrapperTest.java
+++ src/test/java/fish/eyebrow/mugen/HandlerWrapperTest.java
@@ -8,6 +8,8 @@ import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
@@ -28,5 +30,23 @@ final class HandlerWrapperTest {
 
         assertThat(output.isPresent()).isTrue();
         assertEquals(new String(output.get()), expectedOutput, true);
+    }
+
+    @Test
+    void shouldReturnEmptyOptionalWhenGivenBadTestInputJson() {
+        final var badTestInput = FileUtil.readResourceAsBytes("bad_test_input.json");
+
+        final var output = handlerWrapper.handle(badTestInput);
+
+        assertThat(output.isEmpty()).isTrue();
+    }
+
+    @Test
+    void shouldReturnEmptyOptionalWhenGivenNonJsonData() {
+        final var notTestInput = FileUtil.readResourceAsBytes("not_test_input.xml");
+
+        final var output = handlerWrapper.handle(notTestInput);
+
+        assertThat(output.isEmpty()).isTrue();
     }
 }
